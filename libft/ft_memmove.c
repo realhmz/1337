5500036a231c4efd2b5fb832745dff94c1f85130
Hamza Et-Tajani <het-taja@c3r1p10.1337.ma>
2023-11-03 01:29:07 +0100

    W

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -2,22 +2,32 @@
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*temp_dest;
-	unsigned char	*temp_src;
-	size_t			i;
-
-	i = 0;
-	temp_src = (char *)src;
-	temp_dest = (char*)dest;
-	while (i < n)
-	{
-		while (i < n && temp_src[i])
-		{
-			temp_dest[i] = temp_src[i];
-			i++;
-		}
-		i++;	
-	}
-	return (dest);
+	size_t	i;
 	
+	i = 0;
+	if (!dest && !src)
+		return (0);
+	if (dest > src)
+	{
+		while (n-- > 0)
+		{
+			((char *)dest)[n] = ((char *)src)[n];
+		}
+	}
+	else
+		while (i < n)
+		{
+			((char *)dest)[i] = ((char *)src)[i];
+			i++;	
+		}
+	
+	return (dest);
+}
+#include <stdio.h>
+
+int main(){
+	char str[] = "helloooo";
+	char str1[] = "world";
+	ft_memmove(str,str1,0);
+	printf("%s",str);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -25,6 +25,7 @@ int		ft_atoi(const char *nptr);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
+void *ft_memcpy(void *dest, const void *src, size_t n);
 int		ft_isascii(int c);
 char	*ft_strdup(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
