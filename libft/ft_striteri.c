29f9f3c4ace8ef8b1c6fcd333667d5047b12bcfb
Hamza Et-Tajani <het-taja@c4r4p2.1337.ma>
2023-11-11 15:52:54 +0100

    libft

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -1,22 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: het-taja <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/10 17:22:34 by het-taja          #+#    #+#             */
-/*   Updated: 2023/11/10 17:22:35 by het-taja         ###   ########.fr       */
+/*   Created: 2023/11/11 15:14:11 by het-taja          #+#    #+#             */
+/*   Updated: 2023/11/11 15:14:12 by het-taja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	ft_putendl_fd(char *s, int fd)
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	if (s)
+	unsigned int	i;
+
+	i = 0;
+	while (s && s[i])
 	{
-		ft_putstr_fd(s,fd);
-		write(fd, "\n", 1);
+		f(i,&s[i]);
+		i++;
 	}
 }
+
+// #include <stdio.h>
+
+// void f(unsigned int h,char *x)
+// {
+// 	int i = 0;
+// 	x[i] -= 32;
+// }
+// int main() {
+// 	char str[] = "helloworld";
+// 	ft_striteri(str,f);
+// 	printf("%s",str);
+// }
--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -14,5 +14,31 @@
 
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+    int	i;
+
+	i = 	ft_strlen(s);
+	char	*str;
+	if (!(str = malloc(i + 1)))
+		return (0);
+	i = 0;
+	while (str && s[i])
+	{
+		str[i] = f(i,s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return(str);
 }
+
+// #include <stdio.h>
+// char f(unsigned int h , char x )
+// {
+// 	x -= 32;
+
+// 	return x;
+// }
+// int main() {	
+	
+// 	char str[] = "helloworld";
+// 	printf("%s",ft_strmapi(str,f));
+// }
